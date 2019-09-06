6fabca5238fd5a3155ac281f291214fdbd825815
yunkegao <gaoyunke95@gmail.com>
2019-09-06 19:58:05 -0400

    init commit

---
+++ student-helper-demo/src/main/java/com/hcstudenthelper/studenthelperdemo/StudentHelperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hcstudenthelper.studenthelperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHelperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHelperDemoApplication.class, args);
+	}
+
+}
